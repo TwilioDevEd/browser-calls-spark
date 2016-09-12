cb94b9a7297be93d545ee481f639b3d428fe3567
Mario Celi <mceli@stackbuilders.com>
2016-09-12 10:08:03 -0500

    Use twilio-java next-gen version

--- src/test/java/com/twilio/browsercalls/lib/CapabilityTokenGeneratorTest.java
+++ src/test/java/com/twilio/browsercalls/lib/CapabilityTokenGeneratorTest.java
@@ -1,27 +1,39 @@
 package com.twilio.browsercalls.lib;
 
+import com.google.common.collect.Lists;
 import com.twilio.browsercalls.exceptions.UndefinedEnvironmentVariableException;
-import com.twilio.sdk.CapabilityToken;
-import com.twilio.sdk.client.TwilioCapability;
+import com.twilio.jwt.client.ClientCapability;
+import com.twilio.jwt.client.IncomingClientScope;
+import com.twilio.jwt.client.OutgoingClientScope;
+import com.twilio.jwt.client.Scope;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
 public class CapabilityTokenGeneratorTest {
   @Test
   public void testGenerate()
-      throws UndefinedEnvironmentVariableException, CapabilityToken.DomainException {
-    TwilioCapability mockCapability = mock(TwilioCapability.class);
+      throws UndefinedEnvironmentVariableException {
+    ClientCapability.Builder mockCapabilityBuilder = mock(ClientCapability.Builder.class);
+    ClientCapability mockCapability = mock(ClientCapability.class);
     AppSetup mockAppSetup = mock(AppSetup.class);
     CapabilityTokenGenerator generator =
-        new CapabilityTokenGenerator("some role", mockCapability, mockAppSetup);
+        new CapabilityTokenGenerator("some role", mockCapabilityBuilder, mockAppSetup);
+
+    OutgoingClientScope outgoingScope = new OutgoingClientScope.Builder("App ID").build();
+    IncomingClientScope incomingScope = new IncomingClientScope("some role");
+    List<Scope> scopes = Lists.newArrayList(outgoingScope, incomingScope);
 
     when(mockAppSetup.getApplicationSid()).thenReturn("App ID");
+    when(mockCapabilityBuilder.scopes(any())).thenReturn(mockCapabilityBuilder);
+    when(mockCapabilityBuilder.build()).thenReturn(mockCapability);
 
     generator.generate();
 
-    verify(mockCapability).allowClientIncoming("some role");
-    verify(mockCapability).allowClientOutgoing("App ID");
-    verify(mockCapability).generateToken();
+    verify(mockCapabilityBuilder).scopes(any());
+    verify(mockCapabilityBuilder).build();
+    verify(mockCapability).toJwt();
   }
 }
